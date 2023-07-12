f0c8078e6a1834b2544c2c4b0408b9cb1a38139e
Ivan Belyj <ivanbelyj743@gmail.com>
2023-07-13 01:35:36 +0300

    Add Node Value component to layout

--- scripts/init-layout.js
+++ scripts/init-layout.js
@@ -7,20 +7,18 @@ const defaultConfig = {
       content: [
         {
           type: "component",
-          componentName: "causalView",
-          // componentState: { label: "A" },
+          componentName: "Causal View",
         },
         {
           type: "column",
           content: [
             {
               type: "component",
-              componentName: "testComponent",
-              componentState: { label: "B" },
+              componentName: "Node Value",
             },
             {
               type: "component",
-              componentName: "testComponent",
+              componentName: "Test Component",
               componentState: { label: "C" },
             },
           ],
@@ -31,22 +29,43 @@ const defaultConfig = {
 };
 
 export const initLayout = () => {
-  const myLayout = new GoldenLayout(defaultConfig);
-  myLayout.registerComponent(
-    "testComponent",
+  const layout = new GoldenLayout(defaultConfig);
+  layout.registerComponent(
+    "Test Component",
     function (container, componentState) {
       container.getElement().html("<h2>" + componentState.label + "</h2>");
     }
   );
-  myLayout.registerComponent("causalView", causalViewComponent);
-  myLayout.init();
+  layout.registerComponent("Causal View", causalViewComponent);
+  layout.registerComponent("Node Value", nodeValueComponent);
+  layout.on("initialised", () => {
+    initCausalView();
+  });
+  layout.init();
 };
 
 const causalViewComponent = function (container, componentState) {
   const parentDiv = $("<div>").addClass("causal-view");
   $("label");
   container.getElement().append(parentDiv);
-  container.on("open", () => {
-    initCausalView();
-  });
+};
+
+const nodeValueComponent = function (container, componentState) {
+  const element = $(`
+    <div class="component">
+      <div class="text-input text-input_readonly">
+        <label>Id</label>
+        <input type="text" placeholder="Id" id="node-id-input" readonly />
+      </div>
+      <div class="text-input">
+        <label>Title</label>
+        <input type="text" placeholder="Title" id="node-title-input" />
+      </div>
+      <div class="text-input">
+        <label>Node Value</label>
+        <input type="text" placeholder="Node Value" id="node-value-input" />
+      </div>
+      <button id="update-btn">Update</button>
+    </div>`);
+  container.getElement().append(element);
 };
