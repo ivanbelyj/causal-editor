6776fa1501e78d7aadf1454ce55fd62cd549d2b8
Ivan Belyj <ivanbelyj743@gmail.com>
2023-07-12 23:54:08 +0300

    Add golden-layout library

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,54 @@
 import { CausalView } from "./causal-view/causal-view.js";
 import { factsCollection } from "./test-data.js";
 
-function onUpdateButtonClick(causalView) {
-  if (!currentSelectedNodeId) return;
-  const nodeTitleInput = document.getElementById("node-title-input").value;
-  const nodeValueInput = document.getElementById("node-value-input").value;
-  causalView.updateNodeTitleAndValueById(
-    currentSelectedNodeId,
-    nodeTitleInput,
-    nodeValueInput
+(() => {
+  initGoldenLayout();
+  // initCausalView();
+})();
+
+function initGoldenLayout() {
+  const goldenLayoutConfig = {
+    content: [
+      {
+        type: "row",
+        content: [
+          {
+            type: "component",
+            componentName: "testComponent",
+            componentState: { label: "A" },
+          },
+          {
+            type: "column",
+            content: [
+              {
+                type: "component",
+                componentName: "testComponent",
+                componentState: { label: "B" },
+              },
+              {
+                type: "component",
+                componentName: "testComponent",
+                componentState: { label: "C" },
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
+  const myLayout = new GoldenLayout(goldenLayoutConfig);
+  myLayout.registerComponent(
+    "testComponent",
+    function (container, componentState) {
+      container.getElement().html("<h2>" + componentState.label + "</h2>");
+    }
   );
+  myLayout.init();
 }
 
 let currentSelectedNodeId;
 
-(() => {
+function initCausalView() {
   const causalModelNodes = JSON.parse(factsCollection);
   const causalView = new CausalView(".causal-view", causalModelNodes);
   causalView.addEventListener("nodeClicked", (event) => {
@@ -30,7 +64,18 @@ let currentSelectedNodeId;
   document.getElementById("update-btn").onclick = (...data) => {
     onUpdateButtonClick(causalView, ...data);
   };
-})();
+}
+
+function onUpdateButtonClick(causalView) {
+  if (!currentSelectedNodeId) return;
+  const nodeTitleInput = document.getElementById("node-title-input").value;
+  const nodeValueInput = document.getElementById("node-value-input").value;
+  causalView.updateNodeTitleAndValueById(
+    currentSelectedNodeId,
+    nodeTitleInput,
+    nodeValueInput
+  );
+}
 
 function testUpdateFirstNode(causalView) {
   const firstNode = causalView.causalModelNodes[0];
