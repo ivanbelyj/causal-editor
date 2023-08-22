379627254d6cafee97b6868dd158b5db521605c2
Ivan Belyj <ivanbelyj743@gmail.com>
2023-08-23 00:28:25 +0300

    Add clear id / cancel select switching buttons

--- src/js/elements/select-node-element.js
+++ src/js/elements/select-node-element.js
@@ -20,16 +20,64 @@ export class SelectNodeElement {
       .attr("readonly", true)
       .property("value", initialId ?? "");
 
-    const selectNodeButton = this.component
+    this.selectNodeButton = this.component
       .append("button")
       .attr("class", "button")
       .text("Select")
-      .on("click", this.onClick.bind(this));
+      .on("click", this.onSelectButtonClick.bind(this));
+
+    this.clearButton = this.component
+      .append("button")
+      .attr("class", "button select-node-element__input")
+      .text("Clear")
+      .on("click", this.onClearButtonClick.bind(this));
+
+    this.cancelSelectButton = this.component
+      .append("button")
+      .attr("class", "button")
+      .text("Cancel")
+      .on("click", this.onCancelButtonClick.bind(this));
+
+    this.setButtonsVisible(true);
   }
 
-  onClick(event) {
-    console.log("select node button is clicked. event", event);
+  setButtonsVisible(showSelectButtonOnly) {
+    this.selectNodeButton.style(
+      "display",
+      showSelectButtonOnly ? "inline-block" : "none"
+    );
+    this.clearButton.style(
+      "display",
+      !showSelectButtonOnly ? "inline-block" : "none"
+    );
+    this.cancelSelectButton.style(
+      "display",
+      !showSelectButtonOnly ? "inline-block" : "none"
+    );
+  }
 
+  onCancelButtonClick(event) {
+    this.cancelSelect();
+  }
+
+  onClearButtonClick(event) {
+    this.setIdInputAndCancelSelect("");
+  }
+
+  onNodeClicked(event) {
+    const causalModelFact = event.data.i.data;
+    this.setIdInputAndCancelSelect(causalModelFact.Id);
+  }
+
+  setIdInputAndCancelSelect(causalModelFactId) {
+    this.idInput.property("value", causalModelFactId);
+
+    this.cancelSelect();
+
+    this.onNodeIdSelected?.(causalModelFactId);
+  }
+
+  onSelectButtonClick(event) {
     // The last click is to select the cause id, not to select the node to edit
     this.causalView.selectionManager.isSelectByClick = false;
 
@@ -37,19 +85,16 @@ export class SelectNodeElement {
       "nodeClicked",
       this.onNodeClicked // already bound to this
     );
+
+    this.setButtonsVisible(false);
   }
 
-  onNodeClicked(event) {
-    const causalModelFact = event.data.i.data;
-
-    this.idInput.property("value", causalModelFact.Id);
-
+  cancelSelect() {
     this.causalView.structure.removeEventListener(
       "nodeClicked",
       this.onNodeClicked // the same function as in addEventListener
     );
     this.causalView.selectionManager.isSelectByClick = true;
-
-    this.onNodeIdSelected?.(causalModelFact.Id);
+    this.setButtonsVisible(true);
   }
 }
